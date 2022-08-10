8580fd0fcd3a06d0a5bdc7549b081223473d2ece
aaRock766 <aarock766@gmail.com>
2022-08-10 22:59:32 +0800

    20220810 Home fiexd urls homepage

--- mtweb_a/urls.py
+++ mtweb_a/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import mtweb
+from mtweb import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index', mtweb.views),
+    path('', views.index),
 ]
