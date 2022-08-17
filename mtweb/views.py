2497a1cc978efd072d9ff92537115fdd6c94a224
aaRock766 <aarock766@gmail.com>
2022-08-17 23:15:44 +0800

    20220817 home

--- mtweb/views.py
+++ mtweb/views.py
@@ -3,7 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    # return reverse('mtweb:index')
-    return redirect(reverse('mtweb:index'))
-    # return HttpResponse("aaaaa")
-    # return render(request,'index.html')
+    return render(request,'index.html')
